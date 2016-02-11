c071a3418823b74e29c9d4ce8204ad293caea5e4
toyknight <toyknight@outlook.com>
2016-02-11 14:49:54 -0500

    update

---
+++ app/src/main/java/com/njit/buddy/app/widget/CategorySelector.java
@@ -0,0 +1,15 @@
+package com.njit.buddy.app.widget;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+/**
+ * @author toyknight 2/11/2016.
+ */
+public class CategorySelector extends LinearLayout {
+
+    public CategorySelector(Context context) {
+        super(context);
+    }
+
+}
